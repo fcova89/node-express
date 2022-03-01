69d6f72e106a7a58d87f4a459cf52ed3374e60c6
Cist1 <fcova89@gmail.com>
2022-03-01 21:05:13 +0100

    5

--- myApp.js
+++ myApp.js
@@ -1,8 +1,11 @@
 var express = require('express');
+const path require('path');
+
 var app = express();
+
 app.get("/",function(req, res) {
-  res.send(__dirname/views);
-  //res.sendFile(__dirname/views/index.html);
+  res.sendFile(path.join(__dirname, '/views/index.html'));
+    //res.sendFile(__dirname/views/index.html);
 })
 
 
