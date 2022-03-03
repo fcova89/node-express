82336bf9740b7056c948647288fd23200204a0f8
Cist1 <fcova89@gmail.com>
2022-03-03 18:54:04 +0100

    Ripristinato vecchio codice + test con console log

--- myApp.js
+++ myApp.js
@@ -1,32 +1,23 @@
 var express = require('express');
 const path = require('path');
-//require('dotenv').config()
-var jsonObj = {message: "Hello json"};
 
 var app = express();
 
-//app.listen(3000, function(req, res) {console.log(process.env.MESSAGE_STYLE);}); //per utilizzo locale
-
 app.get("/",function(req, res) {
   res.sendFile(path.join(__dirname, '/views/index.html'));
+    //res.sendFile(__dirname/views/index.html);
 })
-
-app.use('/public',express.static(path.join(__dirname, '/public'))); //middleware per utilizzare style.css
+app.use('/public',express.static(path.join(__dirname, '/public')));
 
 app.get('/json', (req, res) => {
   //res.setHeader('Content-Type', 'application/json'); // da verificare per impostare contenuto
   if(process.env.MESSAGE_STYLE == 'uppercase'){
-    var newJsonObj = jsonObj.message.toUpperCase();
-    res.json(newJsonObj);
-    res.send(process.env.MESSAGE_STYLE);
+    var jsonObj.message = jsonObj.message.toUpperCase();
+    console.log(jsonObj);
+    res.json(jsonObj);
   }
-  else{
-    res.json(newJsonObj);
+    res.json(jsonObj);
   }
-
-});
-
-
 
 
 
