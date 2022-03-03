242f5e5ec500f800892bae8dc629513bb0c49457
fcova89 <81139841+fcova89@users.noreply.github.com>
2022-03-03 19:00:49 +0100

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -8,12 +8,3 @@ app.get("/",function(req, res) {
     //res.sendFile(__dirname/views/index.html);
 })
 app.use('/public',express.static(path.join(__dirname, '/public')));
-/*
-app.get('/json', (req, res) => {
-  //res.setHeader('Content-Type', 'application/json'); // da verificare per impostare contenuto
-  if(process.env.MESSAGE_STYLE == 'uppercase'){
-    jsonObj.message = jsonObj.message.toUpperCase();
-    res.json(jsonObj);
-  }
-    res.json(jsonObj);
-});
