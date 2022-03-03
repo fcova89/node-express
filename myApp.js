82c99ad0dd10569f3023fb1fd8558ba5e745acc7
fcova89 <81139841+fcova89@users.noreply.github.com>
2022-03-03 18:59:49 +0100

    Update myApp.js
    eliminato parte json

--- myApp.js
+++ myApp.js
@@ -8,12 +8,11 @@ app.get("/",function(req, res) {
     //res.sendFile(__dirname/views/index.html);
 })
 app.use('/public',express.static(path.join(__dirname, '/public')));
-
+/*
 app.get('/json', (req, res) => {
   //res.setHeader('Content-Type', 'application/json'); // da verificare per impostare contenuto
   if(process.env.MESSAGE_STYLE == 'uppercase'){
     jsonObj.message = jsonObj.message.toUpperCase();
-    console.log(jsonObj);
     res.json(jsonObj);
   }
     res.json(jsonObj);
