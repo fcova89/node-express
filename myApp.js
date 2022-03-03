ad2af21c4e71de21585e8dad249a57ebf8f01165
Cist1 <fcova89@gmail.com>
2022-03-03 05:51:28 +0100

    2

--- myApp.js
+++ myApp.js
@@ -1,8 +1,12 @@
 var express = require('express');
 const path = require('path');
-var http = require('http');
+//var http = require('http');
 
 var app = express();
+
+app.listen(3000, function(req, res) {
+  console.log("Server is running at port 3000");
+});
 
 app.get("/",function(req, res) {
   res.sendFile(path.join(__dirname, '/views/index.html'));
@@ -12,7 +16,7 @@ app.get("/",function(req, res) {
 app.use('/public',express.static(path.join(__dirname, '/public')));
 
 app.get('/json', (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
+  //res.setHeader('Content-Type', 'application/json');
   res.json({message: "Hello json"});
 });
 
