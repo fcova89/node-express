e455fabef0b51f23d0ef18afb9c34350e7c5bc3c
Cist1 <fcova89@gmail.com>
2022-03-03 18:39:31 +0100

    2

--- myApp.js
+++ myApp.js
@@ -9,13 +9,12 @@ var app = express();
 
 app.get("/",function(req, res) {
   res.sendFile(path.join(__dirname, '/views/index.html'));
-    //res.sendFile(__dirname/views/index.html);
 })
 
 app.use('/public',express.static(path.join(__dirname, '/public'))); //middleware per utilizzare style.css
 
 app.get('/json', (req, res) => {
-  //res.setHeader('Content-Type', 'application/json'); // da verificare
+  //res.setHeader('Content-Type', 'application/json'); // da verificare per impostare contenuto
   if(process.env.MESSAGE_STYLE == 'uppercase'){
     jsonObj.message = jsonObj.message.toUpperCase();
     res.json(jsonObj);
